e11c0e81d21f9f477b2ad4de288cdce83619a6a5
Nam Khuc <khucphuongnam2005@gmail.com>
2025-11-15 21:26:51 +0700

    ADD:BTH05

---
+++ BTH_05/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTH_05.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
