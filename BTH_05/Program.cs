e672c668b09f0ff18a63f95fc6cd254c2683f7b4
Nam Khuc <khucphuongnam2005@gmail.com>
2025-11-18 18:00:21 +0700

    FIX: BTH05

--- BTH_05/Program.cs
+++ BTH_05/Program.cs
@@ -1,7 +1,13 @@
+using BTH_05.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext")));
 
 var app = builder.Build();
 
