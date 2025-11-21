9324d7b7c7cc865b7518a36d295a85273d3828cd
Nam Khuc <khucphuongnam2005@gmail.com>
2025-11-21 21:56:54 +0700

    ADD: BTH_06

---
+++ BTH_06/Models/Enrollment.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BTH_06.Models;
+
+public class Enrollment
+{
+    [Key]
+    public string EnrollmentId { get; set; }
+    public string CourseId { get; set; }
+    public Course Course { get; set; }
+    public string LearnerId { get; set; }
+    public Learner Learner { get; set; }
+    public string Grade { get; set; }
+
+}
---
+++ BTH_06/Models/Learner.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BTH_06.Models;
+
+public class Learner
+{
+    [Key]
+    [Required(ErrorMessage = "Mã học viên không được để trống")]
+    public string LearnerId { get; set; }
+
+
+    public string LastName { get; set; }
+    public string LastMidName { get; set; }
+    public string EnrollmentDate { get; set; }
+    
+    [Required(ErrorMessage = "Vui lòng chọn ngành")]
+    public string MajorID { get; set; }
+    public Major Major { get; set; }
+
+    public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+
+
+}
