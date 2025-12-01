b5185fed6f95be80ea927ce683c39a64d9e7f447
Nam Khuc <khucphuongnam2005@gmail.com>
2025-12-01 23:53:26 +0700

    ADD:DeThiMau02'

---
+++ DeThiMau02/KhucPhuongNam_231230845/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KhucPhuongNam_231230845.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
